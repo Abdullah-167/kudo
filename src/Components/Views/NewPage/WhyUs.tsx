381e9cecd6f3a5c80b445e41e2471275a67b3558
Abdullah-167 <abdullahpadhana503@gmail.com>
2023-09-06 21:00:13 +0500

    commit done

--- src/Components/Views/NewPage/WhyUs.tsx
+++ src/Components/Views/NewPage/WhyUs.tsx
@@ -9,9 +9,9 @@ const WhyUs = () => {
                 <div className=' grid grid-cols-1 md:grid-cols-3  gap-10'>
                     {data.map((item, index) => {
                         return (
-                            <div className=' gap-5'
+                            <div className='flex gap-5'
                                 key={index}>
-                                <Image className=' object-contain pb-14 mx-auto' src={item.imge} alt={'card-img'} width={60} height={60} />
+                                <Image className=' object-contain pb-20 mx-auto' src={item.imge} alt={'card-img'} width={60} height={60} />
                                 <div>
                                     <h3 className='text-xl font-semibold pb-2'>{item.heading}</h3>
                                     <div className='w-20 h-[2px] bg-[#8624e1] mb-3'></div>
